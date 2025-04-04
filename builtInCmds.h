6f40b935a860cc039d30a100437c9872a15bfce7
Joseph G. <allenboey@gmail.com>
2025-04-03 20:34:33 -0500

    Update builtInCmds.h

--- builtInCmds.h
+++ builtInCmds.h
@@ -1,7 +1,7 @@
-void handle_echo (char *input);
-void handle_exit (char *input);
-void handle_type (char *arg);
-void handle_path (char *arg);
-void handle_pwd (char *input);
-void handle_cd (char *input);
-void loopBuiltInCmds(char *arg, char *input);
+void handle_echo (char *input, char **args);
+void handle_exit (char *input, char **args);
+void handle_type (char *arg, char **args);
+void handle_path (char *arg, char **args);
+void handle_pwd (char *input, char **args);
+void handle_cd (char *input, char **args);
+int loopBuiltInCmds(char *arg, char *input, char **args);
