695a1c0fddbc2d437970363d14b9f666a775be2f
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-08 07:00:43 -0500

    Create main.c

---
+++ pwd/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pwd.h"
+
+int main(void) {
+   char *userInput = malloc(50);
+
+   // Create home directory node
+   directory *head = malloc(sizeof(directory));
+   strcpy(head->name, "home");
+   head->isPartOfWorkingDir = 1;
+   head->nextPartOfDir = NULL;
+   head->prevPartOfDir = NULL;
+   directory *tail = head;
+
+   createDirectoryNode(&head, &tail, "nextArea");
+   createDirectoryNode(&head, &tail, "otherArea");
+   createDirectoryNode(&head, &tail, "anotherOne");
+   createDirectoryNode(&head, &tail, "lastOne");
+
+   printWorkingDirectory(head);
+
+   freeDirectory(head, tail);
+}
