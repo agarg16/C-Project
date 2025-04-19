3939cb501cb0e7eb0c6c8d7c8896dea264f73d1e
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-18 23:55:00 -0500

    Update pwd.c
    - Updated to print from linked list rather than environment variable and moved createDirectoryNode and freeDirectory to the directory.c file

--- pwd.c
+++ pwd.c
@@ -5,27 +5,10 @@
 #include "pwd.h"
 
 // Prints out the working directory
-void printWorkingDirectory(void) { printf("%s\n", getenv("USER_DIRECTORY")); }
-
-// Creates a directory node
-void createDirectoryNode(directory **ptrToHead, directory **ptrToTail, char *n) {
-directory *newDirNode = malloc(sizeof(directory));
-   strcpy(newDirNode->name, n);
-   newDirNode->isPartOfWorkingDir = 0;
-   newDirNode->prevPartOfDir = *ptrToTail;
-   newDirNode->nextPartOfDir = NULL;
-   newDirNode->prevPartOfDir->nextPartOfDir = newDirNode;
-   *ptrToTail = newDirNode;
-}
-
-// Frees linked list allocated memory
-void freeDirectory(directory *head, directory *tail) {
-   directory *temp = NULL;
-
-   while(head != NULL) {
-      temp = head->nextPartOfDir;
-      free(head);
-      head = temp;
+void printWorkingDirectory(directory *startingLocation) {
+   for(directory *head = startingLocation; head != NULL && head->isPartOfWorkingDir == 1; head = head->nextPartOfDir) {
+       printf("/%s", head->name);
    }
-   tail = NULL;
+ 
+   printf("\n");
 }
