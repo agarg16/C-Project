6a14f9a659b5fb916d3ef19f4b13ff583fe00539
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-25 08:42:22 -0500

    Update sigint.c
    - Updated to remove unnecessary fork

--- sigint.c
+++ sigint.c
@@ -5,17 +5,12 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include "cmds.h"
 
 /* SIGINT handled by replacing current execvp with a new version,
    ensuring the user stays within mock terminal but allowing them
    to enter a different command */
 void handle_sigint (int sig) {
-    printf("\n");
-   
-    char *args[] = { "./main", "run", "-t", NULL };
-    if(execvp(args[0], args) == -1) {
-        fprintf(stderr, "%s\n", strerror(errno));
-    }
-
-    printf("SIGINT\n");
+   write(1, "\nSIGINT USED\n", sizeof("SIGINT USED\n"));
+   // still need to write what we want to have happen during the counting file
 }
