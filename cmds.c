2efc9bba2a79dcfc0e1eb81d55da324b81891f20
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-25 08:32:47 -0500

    Update cmds.c
    - Reworked printWorkingDirectory to only setenv to the PWD directory (rather than the created "USER_DIRECTORY")
    
    - Updated cd to remove unnecessary code, work with the PWD directory rather than "USER_DIRECTORY", and increase overall readability

--- cmds.c
+++ cmds.c
@@ -3,89 +3,68 @@
 #include <stdlib.h>
 #include "cmds.h"
 
+// Prints the working directory
 void printWorkingDirectory(int forTerminalInput) {
-    if(getenv("USER_DIRECTORY") != NULL) {
-        if(forTerminalInput == 1 && (strlen(getenv("USER_DIRECTORY")) > strlen(getenv("HOME")))) {
-          printf("~");
-          printf("%s", getenv("USER_DIRECTORY") + strlen(getenv("HOME")));
-        }
-        else { printf("%s", getenv("USER_DIRECTORY")); }
+    if(forTerminalInput == 1 && (strlen(getenv("PWD")) > strlen(getenv("HOME")))) {
+      printf("~");
+      printf("%s", getenv("PWD") + strlen(getenv("HOME")));
     }
     else {
-        if(forTerminalInput == 1 && (strlen(getenv("PWD")) > strlen(getenv("HOME")))) {
-          printf("~");
-          printf("%s", getenv("PWD") + strlen(getenv("HOME")));
-        }
-        else {
-          printf("%s", getenv("PWD"));
-        }
+      printf("%s", getenv("PWD"));
     }
 }
 
+// Changes the directory
 void changeDirectory(char *input, directory *head) {
-    int numForwardSlashes = 0;
-    int lastSlash = 0;
-    char word[strlen(input) + 1];
-    strcpy(word, input);
-    word[strlen(input)] = '\0';
-
-  
-    if(input[strlen(input) - 1] == '/') {
-      word[strlen(word) - 1] = '\0'; // Removes the extra forward slash at the end
-      setenv("USER_DIRECTORY", word, 1);
+    if(strlen(input) > 1 && input[strlen(input) - 1] == '/') {
+      input[strlen(input) - 1] = '\0';
     }
     else {
-      setenv("USER_DIRECTORY", word, 1);
+      input[strlen(input)] = '\0';
     }
   
-    char *startWord = getenv("PWD");
-    char *endWord = startWord + 1;
-    directory *tempHead = head;
-    directory *nextPtr = tempHead;
+    char *startWord = input + 1;
+    char *endWord = strchr(startWord, '/');
+//    directory *tempHead = head;
+    directory *nextPtr = head;
 
-    for(directory *i = head; i != NULL && endWord != NULL; i = i->nextPartOfDir) {
-      startWord = endWord;
-      endWord = strchr(endWord, '/');
-      
-      // If there are still directory locations (separated by /) to get through
+    for(directory *i = head; i != NULL; i = i->nextPartOfDir) {
       if(endWord != NULL) {
         char shortenedWord[endWord - startWord + 1]; // Accommodates the '\0' character at the end
-        shortenedWord[endWord - startWord] = '\0';
         strncpy(shortenedWord, startWord, endWord - startWord);
-        startWord = endWord + 1;
-        
+        shortenedWord[endWord - startWord] = '\0';
+
         // Sets isPartOfWorkingDir to 1 if the directory location is included in the user's new requested directory (0 if not)
         for(nextPtr = i; nextPtr != NULL; nextPtr = nextPtr->nextPartOfDir) {
-          if(strcmp(nextPtr->name, shortenedWord) == 0) { // The node name is the same as the specific directory location being looked at
+          // Whether or not the node name is the same as the specific directory location being looked at
+          if(strcmp(nextPtr->name, shortenedWord) == 0) {
             nextPtr->isPartOfWorkingDir = 1;
-            tempHead = nextPtr;
           }
           else {
             nextPtr->isPartOfWorkingDir = 0;
           }
         }
-        endWord = endWord + 1;
+
+        startWord = endWord + 1;
       }
-      else if(strcmp(tempHead->nextPartOfDir->name, startWord) == 0) { // If we are on the last directory location of the user-requested directory
-          tempHead->nextPartOfDir->isPartOfWorkingDir = 1;
-          break;
+      else {
+        i->isPartOfWorkingDir = 1;
+        break;
       }
-    }
-    
-    if(tempHead->nextPartOfDir != NULL) {
-      tempHead->nextPartOfDir->isPartOfWorkingDir = 0;
+
+      if(startWord != NULL) { endWord = strchr(startWord + 1, '/'); }
     }
 
-    char newEnvVarName[strlen(input) + 1];
-    strcpy(newEnvVarName, "");
+      char newEnvVarName[strlen(input) + 1];
+      strcpy(newEnvVarName, "");
 
-    for(directory *ptr = head; ptr != NULL && ptr->isPartOfWorkingDir == 1; ptr = ptr->nextPartOfDir) {
-      strcat(newEnvVarName, "/");
-      strcat(newEnvVarName, ptr->name);
-    }
-    
-    setenv("USER_DIRECTORY", newEnvVarName, 1);
+      for(directory *ptr = head; ptr != NULL && ptr->isPartOfWorkingDir == 1; ptr = ptr->nextPartOfDir) {
+        strcat(newEnvVarName, "/");
+        strcat(newEnvVarName, ptr->name);
+      }
+      
+      setenv("PWD", newEnvVarName, 1);
 }
 
 // Exits the terminal
-void exitTerminal() { exit(1); }
+void exitTerminal() { exit(0); }
