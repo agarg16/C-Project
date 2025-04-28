bba244caff2b926da5817ef0f66fce73eb6e8dd9
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-28 03:14:37 -0500

    Update sigint.h
    - Updated to extend the column space that the comment fills before going to the next line

--- sigint.h
+++ sigint.h
@@ -1,8 +1,7 @@
 #ifndef SIGINT_H
 #define SIGINT_H
 
-/* SIGINT handled by replacing current execvp with a new version,
-   ensuring the user stays within mock terminal but allowing them
+/* SIGINT handled by replacing current process with a new version, ensuring the user stays within mock terminal but allowing them
    to enter a different command */
 void handle_sigint(int sig);
 
