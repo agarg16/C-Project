42700e6de6d3f9882db8a48079724966fac2d702
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-28 03:06:20 -0500

    Update cmds.c
    - Updated to include catNapTimer function

--- cmds.c
+++ cmds.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include "fork.h"
 #include "cmds.h"
+#include "sigint.h"
+#include "catart.h"
 
 // Prints the working directory
 void printWorkingDirectory(int forTerminalInput) {
@@ -64,6 +71,38 @@ void changeDirectory(char *input, directory *head) {
       }
       
       setenv("PWD", newEnvVarName, 1);
+}
+
+// Displays a cat nap timer
+void catNapTimer(int seconds) { // signal interrupt testing
+  char input[5]; // buffer for user input
+  printf("Cat Nap Timer!\n");
+  printf("Enter the number of seconds for the cat to nap: ");
+  fgets(input, sizeof(input), stdin); // Read user input for seconds
+  input[strcspn(input, "\n")] = '\0';  // remove newline
+  seconds = atoi(input); // Convert input string to integer    
+    if (seconds <= 0) {
+      printf("Invalid number of seconds. Please enter a positive integer.\n");
+      printf(" $ "); 
+      return; // Exit the function if the input is invalid
+    }
+
+  printf("\nCat is now snoozing... \n");
+  catAsleep(); // Call the cat art function to show the cat asleep
+  fflush(stdout); // Ensure the output is printed immediately
+
+  for (int i = seconds; i > 0; i--) {
+      if(i > 1) {
+        printf("\rTime left: %d seconds\n", i); // Print countdown (/r moves cursor back to beginning of the line)
+      }
+      else { // if (i == 1)
+          printf("\rTime left: %d second\n", i); // Special case for singular second
+      }
+      fflush(stdout); // Ensure the countdown is printed immediately
+      sleep(1); // Sleep for user specified time in seconds
+  }
+  catAwake(); // Call the cat art function to show the cat awake
+  printf("\nCat is done napping...\n");
 }
 
 // Exits the terminal
