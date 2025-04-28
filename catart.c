701d3d2b04bb5620a45c90a56353d90a2cc5db45
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-28 03:04:54 -0500

    Add files via upload

---
+++ catart.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void catAwake() {
+    printf("\n");
+    printf("         /ヽ  ヽ\n");
+    printf("        |  ◒  ◒| \n");
+    printf("      /` ミ__x ﾉ\n");
+    printf("     /       |\n");
+    printf("    /   ヽ   ﾉ\n");
+    printf("    │   | | |\n");
+    printf("／￣|    | | |\n");
+    printf("(￣ヽ__  ヽ_)_)\n");
+    printf("＼二)\n");
+};
+
+void catAsleep() {
+    printf("\n");
+    printf("     	 ／ヽ   フ\n");
+    printf("        |   _  _| \n");
+    printf("      /` ミ__x  ﾉ\n");
+    printf("     /        |\n");
+    printf("    /   ヽ    ﾉ\n");
+    printf("    │   | | |\n");
+    printf("／￣|    | | |\n");
+    printf("(￣ヽ__  ヽ_)_)\n");
+    printf("＼二)\n");
+};
